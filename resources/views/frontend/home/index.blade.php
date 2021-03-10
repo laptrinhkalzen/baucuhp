167ae415cbcef2d0bc29c0f1449ee2b25e739ef6
laptrinhkalzen <info@kalzen.com>
2021-03-10 22:29:53 +0700

    Minh

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -1,5 +1,403 @@
 @extends('frontend.layouts.master')
 @section('content')
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>UBNDTP</title>
+		<link rel="stylesheet" href="{!!asset('assets/frontend/css/reset.css')!!}">
+		<link rel="stylesheet" href="{!!asset('assets/frontend/css/style.css')!!}">
+		<link rel="stylesheet" href="{!!asset('assets/frontend/css/bootstrap.min.css')!!}">
+		<link rel="stylesheet" href="{!!asset('assets/frontend/css/owl.carousel.min.css')!!}">
+		<link rel="stylesheet" href="{!!asset('assets/frontend/css/owl.theme.default.css')!!}">
+		<script src="{!!asset('assets/frontend/js/jquery.min.js')!!}"></script>
+		<script src="{!!asset('assets/frontend/js/script.js')!!}"></script>
+		<script src="{!!asset('assets/frontend/js/owl.carousel.min.js')!!}"></script>
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
+	</head>
+	<div id="wrapper">
+		<header>
+			<div class="topbar">
+				<div class="container">
+					<span>Thứ hai, ngày 11 tháng 3 năm 2021</span><span>Hòm thư góp ý</span>
+				</div>
+			</div>
+			<div class="banner" style="background-image: url('images/cau-hvt.png');">
+				<h1>ỦY BAN BẦU CỬ THÀNH PHỐ HẢI PHÒNG</h1>
+			</div>
+			<div class="nav">
+				<div class="container">
+					<ul>
+						<li>
+							<img src="icon/home.svg">
+						</li>
+						<li>
+							Tin tức bầu cử
+						</li>
+						<li>
+							Tin tức bầu cử
+						</li>
+					</ul>
+				</div>
+			</div>
+		</header>
+		<body>
+			<div class="section1">
+				<div class="container">
+					<div class="row">
+						<div class="section1-column1 col-6">
+							<div class="column1-firstnews">
+								<img src="images/nxp.jpg">
+								<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.</h2>
+								<span>12:37 03/03/2021</span>
+								<br>
+								<a>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.</a>
+							</div>
+							<div class="column1-secondnews">
+								<div class="container">
+									<div class="row">
+										<div class="col-6">
+											<img src="images/nxp.jpg">
+											<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.</h2>
+										</div>
+										<div class="col-6">
+											<img src="images/nxp.jpg">
+											<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.</h2>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="section1-column2 col-3">
+							<div class="column2-firstnews">
+								<img src="images/nxp.jpg">
+								<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.</h2>
+								<hr>
+							</div>
+							<div class="column2-secondnews">
+								<div class="container">
+									<div class="row">
+										<div class="col-5 wrapper-img">
+											<img src="images/nxp.jpg">
+										</div>
+										<div class="col-7 wrapper-title" >
+											<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.</h2>
+										</div>
+									</div>
+								</div>
+								<hr>
+							</div>
+							<div class="column2-secondnews">
+								<div class="container">
+									<div class="row">
+										<div class="col-5 wrapper-img">
+											<img src="images/nxp.jpg">
+										</div>
+										<div class="col-7 wrapper-title" >
+											<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.</h2>
+										</div>
+									</div>
+								</div>
+								<hr>
+							</div>
+							<div class="column2-secondnews">
+								<div class="container">
+									<div class="row">
+										<div class="col-5 wrapper-img">
+											<img src="images/nxp.jpg">
+										</div>
+										<div class="col-7 wrapper-title" >
+											<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.</h2>
+										</div>
+									</div>
+								</div>
+								<hr>
+							</div>
+							<div class="column2-secondnews">
+								<div class="container">
+									<div class="row">
+										<div class="col-5 wrapper-img">
+											<img src="images/nxp.jpg">
+										</div>
+										<div class="col-7 wrapper-title" >
+											<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.</h2>
+										</div>
+									</div>
+								</div>
+								<hr>
+							</div>
+							<div class="column2-secondnews">
+								<div class="container">
+									<div class="row">
+										<div class="col-5 wrapper-img">
+											<img src="images/nxp.jpg">
+										</div>
+										<div class="col-7 wrapper-title" >
+											<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.</h2>
+										</div>
+									</div>
+								</div>
+								<hr>
+							</div>
+						</div>
+						<div class="section1-column3 col-3">
+							<div class="news-tabs">
+								<ul>
+									<li>Tin mới</li>
+									<li>Tin đọc nhiều</li>
+								</ul>
+							</div>
+							<div class="news" >
+								<img src="icon/clock.svg"><span>10 phút trước</span>
+								<h2>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.
+								</h2>
+								<hr>
+							</div>
+							<div class="news" >
+								<img src="icon/clock.svg"><span>10 phút trước</span>
+								<h2>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.
+								</h2>
+								<hr>
+							</div>
+							<div class="news" >
+								<img src="icon/clock.svg"><span>10 phút trước</span>
+								<h2>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.
+								</h2>
+								<hr>
+							</div>
+							<div class="news" >
+								<img src="icon/clock.svg"><span>10 phút trước</span>
+								<h2>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.
+								</h2>
+								<hr>
+							</div>
+							<div class="news" >
+								<img src="icon/clock.svg"><span>10 phút trước</span>
+								<h2>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.
+								</h2>
+								<hr>
+							</div>
+							<div class="news" >
+								<img src="icon/clock.svg"><span>10 phút trước</span>
+								<h2>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.
+								</h2>
+								<hr>
+							</div>
+							<div class="news" >
+								<img src="icon/clock.svg"><span>10 phút trước</span>
+								<h2>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.
+								</h2>
+								<hr>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="section2">
+				<div class="container">
+					<div class="row">
+						<span>Danh sách ứng cử viên</span>
+						<ul>
+							<li>Quốc hội</li>
+							<li>HĐND Thành phố</li>
+							<li>HĐND Huyện</li>
+							<li>HĐND Xã</li>
+						</ul>
+					</div>
+					<div class="row">
+						<div class="container wrapper-carousel">
+							<div class="owl-carousel owl-theme">
+							    <div class="item candidates">
+							    	<img src="images/nxp.jpg">
+							    	<h3> Nguyễn Văn A</h3>
+							    </div>
+							    <div class="item candidates">
+							    	<img src="images/nxp.jpg">
+							    	<h3> Nguyễn Văn A</h3>
+							    </div>
+							    <div class="item candidates">
+							    	<img src="images/nxp.jpg">
+							    	<h3> Nguyễn Văn A</h3>
+							    </div>
+							    <div class="item candidates">
+							    	<img src="images/nxp.jpg">
+							    	<h3> Nguyễn Văn A</h3>
+							    </div>
+							    <div class="item candidates">
+							    	<img src="images/nxp.jpg">
+							    	<h3> Nguyễn Văn A</h3>
+							    </div>
+							    <div class="item candidates">
+							    	<img src="images/nxp.jpg">
+							    	<h3> Nguyễn Văn A</h3>
+							    </div>
+							    <div class="item candidates">
+							    	<img src="images/nxp.jpg">
+							    	<h3> Nguyễn Văn A</h3>
+							    </div>
+							    <div class="item candidates">
+							    	<img src="images/nxp.jpg">
+							    	<h3> Nguyễn Văn A</h3>
+							    </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="section3">
+				<div class="container">
+					<div class="row">
+						<div class="section3-colum1 col-9">
+							<div class="container">
+								<div class="row">
+									<span>VIDEO</span>
+								</div>
+							</div>
+							<div class="container wrapper-video">
+								<div class="row">
+									<div class="col-7 video-column1">
+										<iframe width="100%" height="345px" src="https://www.youtube.com/embed/tgbNymZ7vqY">
+										</iframe>
+										<h3>Video1</h3>
+									</div>
+									<div class="col-5 video-column2">
+										<div class="container">
+											<div class="row video-box">
+												<iframe width="40%" height="106px" src="https://www.youtube.com/embed/tgbNymZ7vqY">
+												</iframe>
+												<h3>Video1</h3>
+											</div>
+											<div class="row video-box">
+												<iframe width="40%" height="106px" src="https://www.youtube.com/embed/tgbNymZ7vqY">
+												</iframe>
+												<h3>Video1</h3>
+											</div>
+											<div class="row video-box">
+												<iframe width="40%" height="106px" src="https://www.youtube.com/embed/tgbNymZ7vqY">
+												</iframe>
+												<h3>Video1</h3>
+											</div>
+										</div>
 
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="section3-colum2 col-3">
+							<div class="container">
+								<div class="row">
+									<span>Tài liệu</span>
+								</div>
+							</div>
+							<div class="wrapper-documents">
+								<div class="documents">
+									<h3>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.
+									</h3>
+									<hr>
+								</div>
+								<div class="documents">
+									<h3>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.
+									</h3>
+									<hr>
+								</div>
+								<div class="documents">
+									<h3>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.
+									</h3>
+									<hr>
+								</div>
+								<div class="documents">
+									<h3>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.
+									</h3>
+									<hr>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="section4">
+				<div class="container">
+					<div class="row">
+						<div class="section4-colum1 col-9">
+							<div class="container">
+								<div class="row">
+									<span>Kết quả bầu cử</span>
+								</div>
+							</div>
+							<div class="container wrapper-result">
+								<div class="row">
+									<div class="col-4">
+										<img src="images/nxp.jpg">
+										<div class="title">
+										<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.</h3>
+										</div>
+									</div>
+									<div class="col-4">
+										<img src="images/nxp.jpg">
+										<div class="title">
+										<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.</h3>
+										</div>
+									</div>
+									<div class="col-4">
+										<img src="images/nxp.jpg">
+										<div class="title">
+										<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id augue sit amet odio placerat maximus sed sed arcu. Aliquam erat volutpat. Nunc ultricies sem turpis, molestie feugiat risus maximus sed. Etiam id arcu dictum, finibus urna id, placerat sapien. Aliquam dictum finibus nisl, a porttitor felis luctus vitae.</h3>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="section4-colum2 col-3">
+							<div class="container">
+								<div class="row wrapper-button">
+									<span>Góc ảnh</span>
+								</div>
+							</div>
+							<div class="wrapper-gallery">
+								<img src="images/nxp.jpg">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</body>
+		<footer>
+			<div class="container">
+				<div class="row">
+					<span>Ủy ban bầu cử thành phố hải phòng</span>
+				</div>
+			</div>
+		</footer>
+	</div>
+</html>
+<script type="text/javascript">
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+</script>
 
 @stop
